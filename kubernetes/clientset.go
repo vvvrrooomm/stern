3b52b238166d1956fcc4e65b09af5e867205690f
Martin Haaß <martin.haass@connyun.com>
2018-06-06 15:14:29 +0200

    add azure auth plugin & update vendor dependencies

--- kubernetes/clientset.go
+++ kubernetes/clientset.go
@@ -24,6 +24,7 @@ import (
 	// auth providers
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/azure"
 )
 
 // NewClientConfig returns a new Kubernetes client config set for a context
